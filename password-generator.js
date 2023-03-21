c210acd868d293db66c2885fa45826ef5715f3a8
Andika <litageming@gmail.com>
2023-03-21 14:31:00 +0700

    Create password-generator.js

---
+++ password-generator.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Password Generator</title>
+    <script src="password-generator.js"></script>
+  </head>
+  <body>
+    <h1>Password Generator</h1>
+    <div>
+      <button id="generate">Generate Password</button>
+      <span id="password"></span>
+    </div>
+  </body>
+</html>
